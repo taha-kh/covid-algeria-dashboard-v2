a7c7823cf2f459026c0166670599a2ad8fdfc7b7
taha-kh <tah.khelifi@gmail.com>
2020-03-18 10:54:19 +0100

    MAJ Stat 18/03, 10:54

--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -9,11 +9,11 @@ export class CitiesService {
 
   cities: City[] = [
     { nom: 'Ouargla ورقلة', latitude: 31.952921, longitude: 5.334971, order: 2, totalCas: 1, totalMorts: 0, totalGueris: 0 },
-    { nom: 'Blida البليدة', latitude: 36.471592, longitude: 2.825939, order: 1, totalCas: 40, totalMorts: 4, totalGueris: 12 },
+    { nom: 'Blida البليدة', latitude: 36.471592, longitude: 2.825939, order: 1, totalCas: 39, totalMorts: 4, totalGueris: 12 },
     { nom: 'Mascara معسكر', latitude: 35.401910, longitude: 0.140138, order: 3, totalCas: 2, totalMorts: 0, totalGueris: 0 },
     { nom: 'Tizi Ouzou تيزي وزو', latitude: 36.713468, longitude: 4.047377, order: 4, totalCas: 4, totalMorts: 0, totalGueris: 0 },
     { nom: 'Souk Ahras سوق أهراس', latitude: 36.279946, longitude: 7.938946, order: 5, totalCas: 1, totalMorts: 0, totalGueris: 0 },
-    { nom: 'Skikda سكيكدة', latitude: 36.881845, longitude: 6.905612, order: 6, totalCas: 2, totalMorts: 1, totalGueris: 0 },
+    { nom: 'Skikda سكيكدة', latitude: 36.881845, longitude: 6.905612, order: 6, totalCas: 4, totalMorts: 1, totalGueris: 0 },
     { nom: 'Alger الجزائر', latitude: 36.772183, longitude: 3.060068, order: 7, totalCas: 4, totalMorts: 0, totalGueris: 0 },
     { nom: 'Guelma قالمة', latitude: 36.462044, longitude: 7.431173, order: 8, totalCas: 2, totalMorts: 0, totalGueris: 0 },
     { nom: 'Adrar أدرار', latitude: 28.017765, longitude: -0.266468, order: 9, totalCas: 1, totalMorts: 0, totalGueris: 0 },
