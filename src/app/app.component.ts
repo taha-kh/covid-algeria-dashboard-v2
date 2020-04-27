10f3b5fd41ade15914f5306b86a7fef70761e9bc
unknown <tah.khelifi@gmail.com>
2020-04-28 00:59:50 +0200

    Finalisation de la version 2 - Le site ne sera plus maintenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  dateMaj: String  = "22/03/2020 - 11:45";
+  dateMaj: String  = "28/04/2020";
 }
